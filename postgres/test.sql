f027c59776650e490bc81cc4c930ad50922b4043
asullom <asullom@gmail.com>
2021-06-17 09:58:20 -0500

    use

--- postgres/test.sql
+++ postgres/test.sql
@@ -2,6 +2,6 @@ CREATE DATABASE users_prod;
 CREATE DATABASE users_dev;
 CREATE DATABASE users_test;
 
-Use users_test;
+use users_test;
 
 Create table test_tb (id serial PRIMARY KEY, name varchar(20));
